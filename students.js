85d87e70264e16025cf329504c1f734ebf247264
Ahmed Khaliel <148324212+AhmedOskhalil@users.noreply.github.com>
2026-09-20 12:22:00 +0300

    Add   the json file

---
+++ students.js
@@ -0,0 +1,128 @@
+const competitors = [
+
+    {
+        id: 1,
+        name: "محمد حمادة",
+        monthlyPoints: 24,
+        overallPoints: 24,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 2,
+        name: "محمد ماهر الرخ",
+        monthlyPoints: 21,
+        overallPoints: 21,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 3,
+        name: "يوسف أحمد",
+        monthlyPoints: 28,
+        overallPoints: 28,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 4,
+        name: "عمرو عادل",
+        monthlyPoints: 15,
+        overallPoints: 15,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 5,
+        name: "أحمد أنور",
+        monthlyPoints: 27,
+        overallPoints: 27,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 6,
+        name: "عبد الرحمن محمد",
+        monthlyPoints: 28,
+        overallPoints: 28,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 7,
+        name: "محمد السيد علي البدري",
+        monthlyPoints: 28,
+        overallPoints: 28,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 8,
+        name: "محمد محمود الرخ",
+        monthlyPoints: 0,
+        overallPoints: 0,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    },
+
+    {
+        id: 9,
+        name: "حمدي شريف",
+        monthlyPoints: 0,
+        overallPoints: 0,
+        avatar: "",
+        exams: [
+            {
+                title: "امتحان اول درسين في الوحدة الاولي",
+                img: ""
+            }
+        ]
+    }
+];
